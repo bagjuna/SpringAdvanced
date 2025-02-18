7d1e68dfa973069b17faa57ce67545a832b9a244
bagjuna <juna.park1234@gmail.com>
2025-02-18 14:05:18 +0900

    v4

--- src/main/java/hello/advanced/trace/callback/TraceTemplate.java
+++ src/main/java/hello/advanced/trace/callback/TraceTemplate.java
@@ -1,30 +1,27 @@
-package hello.advanced.trace.template;
+package hello.advanced.trace.callback;
 
 import hello.advanced.trace.TraceStatus;
 import hello.advanced.trace.logtrace.LogTrace;
 
-public abstract class AbstartTemplate<T> {
+public class TraceTemplate {
 
     private final LogTrace trace;
 
-    public AbstartTemplate(LogTrace trace) {
+    public TraceTemplate(LogTrace trace) {
         this.trace = trace;
     }
 
-    public T execute(String message) {
-
+    public <T> T execute(String message, TraceCallback<T> callback) {
         TraceStatus status = null;
         try {
             status = trace.begin(message);
-            T result = call();
+            T result = callback.call();
             trace.end(status);
+
             return result;
         } catch (Exception e) {
             trace.exception(status, e);
             throw e;
         }
-
     }
-
-    protected abstract T call();
 }
