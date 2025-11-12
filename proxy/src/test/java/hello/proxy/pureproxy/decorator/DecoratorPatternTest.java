8b26ad6f9027ba26ce485c0e6aa0f8b0b7cb0874
bagjuna <juna.park124@gmail.com>
2025-11-12 14:11:21 +0900

    proxy 준비

---
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,36 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+    @Test
+    void noDecorator() {
+        RealComponent realComponent = new RealComponent();
+        DecoratorPatterClient client = new DecoratorPatterClient(realComponent);
+        client.execute();
+
+    }
+
+    @Test
+    void decorator1() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatterClient client = new DecoratorPatterClient(messageDecorator);
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatterClient client = new DecoratorPatterClient(timeDecorator);
+        client.execute();
+    }
+
+
+}
