1a1b293351c7324ac9e58d2f2c140f9233a73555
bagjuna <juna.park124@gmail.com>
2025-11-14 23:44:03 +0900

    - 리플랙션

--- proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
+++ proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -3,7 +3,6 @@ package hello.proxy.jdkdynamic;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 @Slf4j
@@ -15,62 +14,62 @@ public class ReflectionTest {
 
         // 공통 로직1 시작
         log.info("start");
-        String resultA = target.callA();  // 호출하는 메서드가 다름
-        log.info("resultA={}", resultA);
+        String result1 = target.callA();
+        log.info("result1={}", result1);
         // 공통 로직1 종료
 
         // 공통 로직2 시작
         log.info("start");
-        String resultB = target.callB();  // 호출하는 메서드가 다름
-        log.info("resultB={}", resultB);
+        String result2 = target.callB();
+        log.info("result2={}", result2);
         // 공통 로직2 종료
+
 
     }
 
-
     @Test
-    void reflection1() throws Exception{
+    void reflection1() throws Exception {
         // 클래스 정보
         Class<?> classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
 
         Hello target = new Hello();
-
+        // callA 메서드 정보
         Method methodCallA = classHello.getMethod("callA");
         Object result1 = methodCallA.invoke(target);
         log.info("result1={}", result1);
 
+        // callB 메서드 정보
         Method methodCallB = classHello.getMethod("callB");
         Object result2 = methodCallB.invoke(target);
         log.info("result2={}", result2);
-
     }
 
-
     @Test
-    void reflection2() throws Exception{
+    void reflection2() throws Exception {
         // 클래스 정보
         Class<?> classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
 
         Hello target = new Hello();
-
         // callA 메서드 정보
         Method methodCallA = classHello.getMethod("callA");
         dynamicCall(methodCallA, target);
-
-        // callA 메서드 정보
+        // callB 메서드 정보
         Method methodCallB = classHello.getMethod("callB");
         dynamicCall(methodCallB, target);
+
     }
 
-    private void dynamicCall(Method method,Object target) throws Exception {
+
+    private void dynamicCall(Method method, Object target) throws Exception {
         log.info("start");
         Object result = method.invoke(target);
-        log.info("resultA={}", result);
-
+        log.info("result={}", result);
     }
 
     @Slf4j
-    static class Hello{
+    static class Hello {
+
+
         public String callA() {
             log.info("callA");
             return "A";
@@ -81,9 +80,7 @@ public class ReflectionTest {
             return "B";
         }
 
-        public String callC() {
-            log.info("callC");
-            return "C";
-        }
     }
+
+
 }
